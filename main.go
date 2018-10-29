f20b7d756e6514b632ecfcf00965811a6f0924e4
xiaoming.zhang <xiaoming.zhang@fengjr.com>
2018-10-29 11:24:37 +0800

    添加context控制

--- main.go
+++ main.go
@@ -3,25 +3,36 @@ package main
 import (
 	"fmt"
 	"github.com/learnergo/simplequeue/queue"
+	"golang.org/x/net/context"
 	"time"
 )
 
 var chanEnqueue = make(chan int)
 
-func genkey() {
+func genkey(ctx context.Context) {
 	i := 0
 	go func() {
 		for {
-			chanEnqueue <- i
-			i++
+			select {
+			case <-ctx.Done():
+				fmt.Println("genkey exit!")
+				return
+			default:
+				chanEnqueue <- i
+				i++
+			}
+
 		}
 	}()
 }
 
-func enqueue(queue queue.Queue) {
+func enqueue(ctx context.Context, queue queue.Queue) {
 	go func() {
 		for {
 			select {
+			case <-ctx.Done():
+				fmt.Println("enqueue exit")
+				return
 			case value := <-chanEnqueue:
 				queue.Enqueue(value)
 			default:
@@ -30,12 +41,19 @@ func enqueue(queue queue.Queue) {
 	}()
 }
 
-func dequeue(queue queue.Queue) {
+func dequeue(ctx context.Context, queue queue.Queue) {
 	go func() {
 		for {
-			if result, ok := queue.Dequeue(); ok {
-				fmt.Println(result)
+			select {
+			case <-ctx.Done():
+				fmt.Println("dequeue exit")
+				return
+			default:
+				if result, ok := queue.Dequeue(); ok {
+					fmt.Println(result)
+				}
 			}
+
 		}
 	}()
 }
@@ -43,10 +61,17 @@ func dequeue(queue queue.Queue) {
 func main() {
 	queue := queue.NewQueue()
 
-	genkey()
-	enqueue(queue)
-	dequeue(queue)
+	ctx1, cancel1 := context.WithCancel(context.Background())
+	ctx2, cancel2 := context.WithCancel(context.Background())
+	ctx3, cancel3 := context.WithCancel(context.Background())
+	genkey(ctx1)
+	enqueue(ctx2, queue)
+	dequeue(ctx3, queue)
 
-	time.Sleep(5 * time.Second)
+	time.Sleep(2 * time.Second)
+	cancel1()
+	cancel2()
+	cancel3()
+	time.Sleep(1 * time.Second)
 	fmt.Println("done")
 }
